6111457cfdbad9bfb1fe8963c15516ee5abc319c
Ilya Rautkin <122689830+pedronin@users.noreply.github.com>
2023-08-31 14:50:15 +0300

    исправил ошибку роутинга с помощью basename

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 import App from './App';
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>,
